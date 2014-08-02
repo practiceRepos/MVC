5eeee5e9e286a795bf6bb46c5c5d3ba317acbbcf
Paul Shannon <p.shannonire@gmail.com>
2014-08-02 21:03:06 +0100

    Initial commit

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller //a controller, home controller inherits from the controler class
+    {
+        //action method, when somebody calls into a controller this will spin up
+        //this method here will go to the views folder,the home folder because you are in the HomeController class and look for Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
